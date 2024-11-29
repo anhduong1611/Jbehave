f401f9c31046cdb9bb4fc7c37b01ead6483f2dc4
anhduong1611 <hothianhduong16112002@gmail.com>
2024-11-29 13:00:55 +0700

    Test Jenkins CI 123

--- src/test/java/steps/phone/GetListPhoneSteps.java
+++ src/test/java/steps/phone/GetListPhoneSteps.java
@@ -9,25 +9,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import utils.JsonUtils;
 
+import java.util.Objects;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GetListPhoneSteps {
-
-    private RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate();
     private ResponseEntity<String> response;
-    private String URL = GlobalVariables.getUrl();
     @Given("I fetch list phone")
     public void givenIFetchListUser() {
-            response = restTemplate.getForEntity(URL+"/objects", String.class);
+            response = restTemplate.getForEntity(GlobalVariables.getUrl()+"/objects", String.class);
     }
 
     @Then("response list of phones")
     public void thenResponseListOfUser() throws JsonProcessingException {
         assertEquals(200,response.getStatusCodeValue(),"Verify status code is 200");
         HttpHeaders headers = response.getHeaders();
-        assertEquals("application/json",headers.getContentType().toString(), "Verify content type");
+        assertEquals("application/json", Objects.requireNonNull(headers.getContentType()).toString(), "Verify content type");
         Set<String> attributes = Set.of("id","name","data");
         JsonUtils.verifyAttributesJsonArray(response.getBody(),attributes);
     }
